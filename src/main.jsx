6e43557a81b6068d5947f9cc4f4b708e599294d4
HarshGupta <harshgupta06dev@gmail.com>
2025-11-28 08:38:19 +0530

    routes is defined

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import TransactionsPage from "./Transaction.jsx";
-import "./index.css";
-import Analytics from "./Analytics.jsx";
-import DebtTracker from "./Debt.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-    <TransactionsPage />
-    <Analytics />
-    <DebtTracker />
-  </StrictMode>
+  </BrowserRouter>
 );
