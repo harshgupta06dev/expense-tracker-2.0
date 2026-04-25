ffd5511115f70cf03a756ed4d1a5bd0cfe770fff
HarshGupta <harshgupta06dev@gmail.com>
2026-04-25 11:37:02 +0530

    some authentication fixxed

--- src/Features/Authentication/SignUp.jsx
+++ src/Features/Authentication/SignUp.jsx
@@ -1,4 +1,4 @@
-import { supabase } from "../supabaseClient";
+import { supabase } from "../../Supabase-Client";
 import React, { useState } from "react";
 import {
   Eye,
@@ -14,6 +14,7 @@ import {
   Sparkles,
   CheckCircle2,
 } from "lucide-react";
+
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
