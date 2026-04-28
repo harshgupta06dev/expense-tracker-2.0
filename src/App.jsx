ca881f6fffdf36f61789bc5dfa37517ee9da6f0e
HarshGupta <harshgupta06dev@gmail.com>
2026-04-28 10:33:48 +0530

    Performance upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./Features/Dashboard/Dashboard";
-import TransactionsPage from "./Features/Transaction/Transaction";
-import Analytics from "./Features/Analytics/Analytics";
-import Debt from "./Features/Debt/Debt";
-import SignUp from "./Features/Authentication/SignUp";
-import LoginPage from "./Features/Authentication/Login";
-
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { supabase } from "./Supabase-Client";
+
+// Lazy imports
+const Dashboard = lazy(() => import("./Features/Dashboard/Dashboard"));
+const TransactionsPage = lazy(
+  () => import("./Features/Transaction/Transaction"),
+);
+const Analytics = lazy(() => import("./Features/Analytics/Analytics"));
+const Debt = lazy(() => import("./Features/Debt/Debt"));
+const SignUp = lazy(() => import("./Features/Authentication/SignUp"));
+const LoginPage = lazy(() => import("./Features/Authentication/Login"));
+
 function App() {
   const [session, setSession] = useState(null);
 
@@ -19,7 +23,6 @@ function App() {
 
     getSession();
 
-    // Listen for auth changes
     const { data: listener } = supabase.auth.onAuthStateChange(
       (_event, session) => {
         setSession(session);
@@ -32,27 +35,27 @@ function App() {
   }, []);
 
   return (
-    <Routes>
-      {/* If NOT logged in */}
-      {!session && (
-        <>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </>
-      )}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {!session && (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
 
-      {/* If logged in */}
-      {session && (
-        <>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/transactions" element={<TransactionsPage />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/debt" element={<Debt />} />
-          <Route path="*" element={<Navigate to="/dashboard" />} />
-        </>
-      )}
-    </Routes>
+        {session && (
+          <>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/transactions" element={<TransactionsPage />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/debt" element={<Debt />} />
+            <Route path="*" element={<Navigate to="/dashboard" />} />
+          </>
+        )}
+      </Routes>
+    </Suspense>
   );
 }
 
